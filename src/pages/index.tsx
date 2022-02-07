5e9e5a5f8c490bf932abe57b6b111989e44257e9
katou <mikato925@gmail.com>
2022-02-07 17:17:35 +0900

    [update] delete Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import { Heading , Button, ButtonGroup, Stack } from '@chakra-ui/react';
+import { Heading, Button, ButtonGroup, Stack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -62,19 +61,6 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
